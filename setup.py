0cb44bee975cadef6ccc099d82ddf10ce555bd6a
jenazzad <mansaryouness@gmail.com>
2021-04-10 21:01:15 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 
 try:
-    with open(os.path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), encoding="utf-8") as f:
         REQUIRED = f.read().split("\n")
 except:
     REQUIRED = []
