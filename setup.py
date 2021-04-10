54b02719311a8f7d5808a6d148c5aee4a039feba
jenazzad <mansaryouness@gmail.com>
2021-04-10 20:59:02 +0200

    fix README.md

--- setup.py
+++ setup.py
@@ -11,6 +11,12 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
+try:
+    with open(os.path.join(here, "requirements.txt"), encoding="utf-8") as f:
+        REQUIRED = f.read().split("\n")
+except:
+    REQUIRED = []
+
 setup(
     name="smiles_mol",
     version="0.0.1",
@@ -18,7 +24,8 @@ setup(
     author_email="mansaryounessecp@gmail.com",
     description="Predict Molecular properties",
     license="MIT",
-    keywords="tabular",
+    install_requires=REQUIRED,
+    keywords="Molecular",
     url="https://github.com/CVxTz/molecule_properties_from_smiles",
     packages=["smiles_mol"],
     entry_points={"console_scripts": ["smiles_mol=smiles_mol.main:cli"]},
